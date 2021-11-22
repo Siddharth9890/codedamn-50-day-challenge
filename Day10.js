97838e30ae217ad3e66e0fae6eb24eef66421f8d
Siddhartha9890 <siddharth.a9890@gmail.com>
2021-11-22 15:47:04 +0530

    done with day 11 challenge

---
+++ Day10.js
@@ -0,0 +1,17 @@
+const array = [
+    { field: "id", operator: "eq" },
+    { field: "cStatus", operator: "eq" },
+    { field: "money", operator: "eq" },
+];
+
+const filterField = "money"
+
+function removeArrayElement(filterField) {
+    // write your solution here
+myArray = array.filter(function( obj ) {
+    return obj.field !== filterField;
+});
+    return myArray;
+}
+
+console.log(`filtered array: ${removeArrayElement(filterField)}`)
---
+++ Day11.js
@@ -0,0 +1,8 @@
+function fibonacci(n) {
+    if(n==0||n==1)
+        return n;
+    
+	return fibonacci(n-1)+fibonacci(n-2);
+}
+
+console.log(`fibonacci value at position 5: ${fibonacci(5)}`)
